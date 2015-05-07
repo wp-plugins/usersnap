66d26e8cec3fe86c524f14568dd0ec0d099f1508
usersnap <usersnap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-07 12:25:40 +0000

    version 4.0
    git-svn-id: https://plugins.svn.wordpress.org/usersnap/trunk@1155310 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usersnap.php
+++ usersnap.php
@@ -3,13 +3,13 @@
 Plugin Name: Usersnap
 Plugin URI: http://www.usersnap.com
 Description: Usersnap helps website owners to get feedback in form of screenshots from their customers, readers or users.
-Version: 3.2
+Version: 4.0
 Author: Usersnap
 Author URI: http://usersnap.com
 License: GPL v2
 */
 
-define('USERSNAP_VERSION', '3.2');
+define('USERSNAP_VERSION', '4.0');
 define('USERSNAP_POINTER_VERSION', '0_1');
 define('USERSNAP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
